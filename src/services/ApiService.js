4eece851ccb020c60207bf73ea74431ffbd88338
tilakdeveloper <tilakdevelopment@gmail.com>
2025-09-13 18:25:56 +0530

    updated backend endpoint

--- src/services/ApiService.js
+++ src/services/ApiService.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export default class ApiService {
 
 
-    static BASE_URL = import.meta.env.VITE_BACKEND_URL;
+    static BASE_URL = import.meta.env.VITE_BACKEND_URL + import.meta.env.VITE_BACKEND_API_VERSION;
 
     static saveToken(token) {
         localStorage.setItem("token", token);
